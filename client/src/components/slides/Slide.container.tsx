312a3cc42bd241ac71678426a4613fbddf49c230
boosting-my-adrenaline <komfort.eto.plen@gmail.com>
2022-02-22 21:36:15 +0300

    build added

--- client/src/components/slides/Slide.container.tsx
+++ client/src/components/slides/Slide.container.tsx
@@ -22,21 +22,21 @@ export const SlidesContainer: React.FC<IProps> = ({
 
   const li = (slide: JSX.Element) => (
     <li className={`snap-center`}>
-      <div className={`h-[92vh] w-full border-b border-black  bg-sky-100`}>
+      <div className={`h-[99vh] w-full border-b border-black  bg-sky-100`}>
         {slide}
       </div>
     </li>
   )
   return (
-    <div className={`  relative w-full overflow-visible`}>
+    <div className={`relative w-full overflow-visible`}>
       <ul
-        className={`no-scrollbar snap relative h-[92vh] snap-y snap-mandatory overflow-y-scroll`}
+        className={`no-scrollbar snap relative h-[99vh] snap-y snap-mandatory overflow-y-scroll`}
       >
         {li(<Slide1 />)}
         {li(<h1></h1>)}
         {li(<h1></h1>)}
       </ul>
-      <div className={`h-[8vh]`}></div>
+      <div className={`h-[1vh]`}></div>
     </div>
   )
 }
